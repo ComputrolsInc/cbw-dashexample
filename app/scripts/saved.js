b7a614e90c94f0ec06b8b3c26f8f02f039825a8c
Bryce Jacobs <popasquat89@gmail.com>
2015-06-05 16:07:06 -0500

    styles - add animation for chart, background fixes, header

--- app/scripts/saved.js
+++ app/scripts/saved.js
@@ -1,12 +1,12 @@
 (function (){
-    var margin = {top: 40, right: 20, bottom: 30, left: 75},
-        width = 960 - margin.left - margin.right,
+    var margin = {top: 40, right: 10, bottom: 30, left: 45},
+        width = 825 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
     var barWidth = 500;
 
     var x = d3.scale.ordinal()
-        .rangeRoundBands([0, width], .3);
+        .rangeRoundBands([0, width], .35);
 
     var y = d3.scale.linear()
         .range([height, 0]);
@@ -18,7 +18,7 @@
     var yAxis = d3.svg.axis()
         .scale(y)
         .orient("left")
-        .tickFormat(d3.format('0$'));
+        .tickFormat(d3.format('$'));
 
     var line = d3.svg.line()
         .x(function (d) {return x(d.day)})
@@ -29,10 +29,16 @@
         .attr("height", height + margin.top + margin.bottom)
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    var rectClip = svg.append('clipPath')
+        .attr('id', 'rect-clip')
+        .append('rect')
+        .attr('width', 0)
+        .attr('height', height);
 
     d3.json('saveddata.json', function(error, data) {
       x.domain(data.map(function(d) { return d.day; }));
-      y.domain([0, d3.max(data, function(d) { return d.current > d.last ? d.current : d.last; })]);
+      y.domain([0, d3.max(data, function(d) { return d.current > d.last ? d.current + 3 : d.last + 3; })]);
 
       svg.append("g")
           .attr("class", "x axis")
@@ -49,8 +55,7 @@
           .style("text-anchor", "end")
           .text("Dollars Saved");
 
-    var bars = svg.selectAll(".bar").data(data).enter();
-
+    var bars = svg.selectAll(".savings_svg--bar").data(data).enter();
     bars.append('rect')
         .attr('class', 'savings_svg--bar')
         .attr('x', function(d){ return x(d.day); })
@@ -58,10 +63,11 @@
         .attr('y', function(d){ return d.current < d.last ? y(d.current) : y(d.last); })
         .attr('height', function(d){ 
             return d.current < d.last ? height - y(d.current) : height - y(d.last);
-        });
+        })
+        .attr('clip-path', 'url(#rect-clip)');
         
     var efficiency = bars.append('g').attr('class', 'savings_svg--efficiency');
-    efficiency.append('rect')
+    var efficiencyBar = efficiency.append('rect')
             .attr('class', function (d){
                 return d.current < d.last ? 'efficiency_bar bad' : 'efficiency_bar good';
             })
@@ -73,9 +79,10 @@
             .attr('height', function (d){
                 return d.current < d.last ? height - y(d.last - d.current) : 
                     height - y(d.current - d.last);
-            });
+            })
+            .attr('clip-path', 'url(#rect-clip)');
 
-    efficiency.append('text')
+    var efficiencyLabel = efficiency.append('text')
             .attr('class', function (d){ 
                 return d.current < d.last ? 'efficiency_label bad' : 'efficiency_label good' 
             })
@@ -86,17 +93,18 @@
             .attr('dy', ".71em")
             .attr('dx', function (d){
                 // Center the percentages
-                return x.rangeBand() * 0.22;
+                return x.rangeBand() * 0.10;
             })
             .text(function (d) {
                 var decimals = d.current < d.last ? (d.last-d.current) / d.current : (d.current-d.last) / d.last;
                 var change = d.current < d.last ? '-' : '+';
                 return change + Math.round(decimals.toPrecision(2) * 100) + '%'; 
-            });
+            })
+            .attr('clip-path', 'url(#rect-clip)');
 
     // Line draw
     var lineGroup = svg.append('g')
-        .attr('transform', 'translate(25, 0)')
+        .attr('transform', 'translate(20, 0)')
         .append('path')
         .datum(data)
         .attr('class', 'lastMonthLine')
@@ -106,6 +114,16 @@
         .attr('stroke-width', '1.5px')
         .attr('marker-start', 'url(#markerCircle)')
         .attr('marker-mid', 'url(#markerCircle)')
-        .attr('marker-end', 'url(#markerCircle)');
+        .attr('marker-end', 'url(#markerCircle)')
+        .attr('clip-path', 'url(#rect-clip)');
     });
+
+    function animateChart(){
+        rectClip.transition()
+            .ease('linear')
+            .duration(3500)
+            .attr('width', width);
+    }
+
+    animateChart();
 })();
